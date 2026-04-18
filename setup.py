eb702404f1e9c517a63a02b34c4da76fb30912e6
Umer Saeed <umersaeed81@hotmail.com>
2026-04-18 16:05:14 +0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="file-filter-utils",
+    version="1.0.0",
+    description="Flexible file filtering based on date extraction from filenames",
+    author="Umer Saeed",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
